bbd377f541dbec66cd06dd2b618adb8a1fb41da5
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-03 18:00:37 -0400

    add seed data

--- seeds/4initial_templates_options.js
+++ seeds/4initial_templates_options.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('template_options').insert([
-        { template_id: 1, option_id: 1, option_name: 'DISPOSITION', option_type: 'boolean', option_text: "input string" },
-        { template_id: 2, option_id: 77, option_name: 'DISPOSITION', option_type: 'integer', option_text: "input string" },
-        { template_id: 1, option_id: 22, option_name: 'DISPOSITION', option_type: 'boolean', option_text: "input string" },
+        { template_id: 1, option_id: 1, option_name: 'NAME', option_type: 'string', option_text: "Little Timmy" },
+        { template_id: 1, option_id: 2, option_name: 'AGE', option_type: 'number', option_text: "7" },
+        { template_id: 1, option_id: 3, option_name: 'DISPOSITION', option_type: 'dropdown', option_text: "[naughty,nice]" },
       ]);
     });
 };
