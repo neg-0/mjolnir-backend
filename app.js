2f55e6e25a05b9001e6b22c9aa02b5ee5f3915f2
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-03 13:48:14 -0400

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const knex = require('knex')(require('./knexfile.js')[process.env.NODE_ENV || 'development']);
+const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("this is the Home screen")
