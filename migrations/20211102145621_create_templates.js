6d892af1a968689c929b7962108419fe79e90aa2
Mario <matthew.davies.3@spaceforce.mil>
2021-11-06 19:34:18 -0400

    insert comment

--- migrations/20211102145621_create_templates.js
+++ migrations/20211102145621_create_templates.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('templates', table => {
     table.increments('id'); // adds an auto incrementing PK column
     table.varchar("title").notNullable();
-    table.string("body", 100000);
+    table.string("body", 100000);//default max size 255
     table.timestamps(true, true); // adds created_at and updated_at
   });
 }
