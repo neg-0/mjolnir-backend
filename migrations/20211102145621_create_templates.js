6bbda87735054c8c86876a0418bc38944f4b3a2b
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-05 20:10:38 -0400

    change body to string

--- migrations/20211102145621_create_templates.js
+++ migrations/20211102145621_create_templates.js
@@ -1,10 +1,10 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('templates', table => {
+  return knex.schema.createTable('templates', table => {
     table.increments('id'); // adds an auto incrementing PK column
     table.varchar("title").notNullable();
-    table.varchar("body", 10000);
+    table.string("body", 100000);
     table.timestamps(true, true); // adds created_at and updated_at
-});
+  });
 }
 
 exports.down = function (knex) {
