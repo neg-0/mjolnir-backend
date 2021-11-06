97f88504f28be697391eb3df72eb2dfdbfb7c0bc
Mario <matthew.davies.3@spaceforce.mil>
2021-11-06 19:34:00 -0400

    insert hex comment

--- migrations/20211101215240_create_users.js
+++ migrations/20211101215240_create_users.js
@@ -2,7 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', table => {
     table.increments('id'); // adds an auto incrementing PK column
     table.string('user_name').notNullable();
-    table.varchar('password', 64);
+    table.varchar('password', 64);//will be passed as hex
     table.timestamps(true, true); // adds created_at and updated_at
   });
 }
