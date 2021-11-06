4f344499ea652adc52f081f0958f7d9b7ebba1b9
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-05 22:11:58 -0400

    capitalized tag names

--- seeds/3initial_users_templates.js
+++ seeds/3initial_users_templates.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users_templates').insert([
-        { user_id: 1, template_id: 1, file_name: "Floyd's First Letter", serialized_options: { name: "Floyd" } }, // ID 1
-        { user_id: 2, template_id: 1, file_name: "First Letter to Santa", serialized_options: { name: "ITS A ME, MARIO" } }, // ID 2
-        { user_id: 1, template_id: 1, file_name: "Floyd's Second Letter", serialized_options: { name: "Also Floyd" } } // ID 3
+        { user_id: 1, template_id: 1, file_name: "Floyd's First Letter", serialized_options: { NAME: "Floyd" } }, // ID 1
+        { user_id: 2, template_id: 1, file_name: "First Letter to Santa", serialized_options: { NAME: "ITS A ME, MARIO" } }, // ID 2
+        { user_id: 1, template_id: 1, file_name: "Floyd's Second Letter", serialized_options: { NAME: "Also Floyd" } } // ID 3
       ]);
     });
 }
