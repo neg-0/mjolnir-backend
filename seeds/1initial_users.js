92450e2ef98e29e27cb0608eb338a3f57ac9fd0c
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-11-07 18:24:34 -0500

    added password hash to user seeds

--- seeds/1initial_users.js
+++ seeds/1initial_users.js
@@ -5,9 +5,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        { user_name: 'Floyd' , password: "Floyd"},
-        { user_name: 'Mario' , password: "Mario"},
-        { user_name: 'Stephen' , password: "Stephen" }
+        { user_name: 'Floyd', password: "1e3974507e8d6e1a670414a6d07e98911d9cf600c946138faeb01df81cd124ad" },
+        { user_name: 'Mario', password: "1e3974507e8d6e1a670414a6d07e98911d9cf600c946138faeb01df81cd124ad" },
+        { user_name: 'Stephen', password: "1e3974507e8d6e1a670414a6d07e98911d9cf600c946138faeb01df81cd124ad" }
       ]);
     });
 };
